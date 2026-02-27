fe49cc9020b5645f5b92806ab8a25de77fa5aa3d
codetestingtech-qa <codetesting.tech@gmail.com>
2026-02-27 18:30:28 -0300

    f

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   async rewrites() {
     return [
       {
         source: "/assets/:path*",
-        destination: "https://conviveapp.lovable.app/assets/:path*",
+        destination: "https://conviveapp.com.br/assets/:path*",
       },
     ];
   },
 };
+
+module.exports = nextConfig;
